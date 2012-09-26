354e825208af8dc9134eb6484d9da7c94a8ff970
Herman Moreno <herman.moreno@crowdint.com>
2012-09-26 16:03:19 -0500

    Parse index.haml file in the packing process

--- lib/chrome_packager/application.rb
+++ lib/chrome_packager/application.rb
@@ -1,6 +1,18 @@
 require 'haml'
 require 'sinatra'
 
+module Haml
+  module Helpers
+    def stylesheet_link_tag(asset)
+      "<link href='/assets/#{asset}.css' media='screen' rel='stylesheet' type='text/css'>"
+    end
+
+    def javascript_include_tag(asset)
+      "<script src='/assets/#{asset}.js'></script>"
+    end
+  end
+end
+
 module ChromePackager
   class Application < Sinatra::Base
     set :root, 'app'
